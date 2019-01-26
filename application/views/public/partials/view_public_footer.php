51843e524473ae77d4eab06b00aad39e7f755064
dwihardyanti <ryancasper68@gmail.com>
2019-01-26 13:50:14 +0800

    membuat view halaman depan

---
+++ application/views/public/partials/view_public_footer.php
@@ -0,0 +1,37 @@
+<div class="footer w3layouts">
+	<div class="container">
+		<div class="footer-top-at w3">
+			
+			<div class="col-md-3 amet-sed w3l">
+				<h4>MORE INFO</h4>
+				<ul class="nav-bottom">
+					<li><a href="#">How to order</a></li>
+					<li><a href="#">FAQ</a></li>
+					<li><a href="contact.html">Location</a></li>
+					<li><a href="#">Shipping</a></li>
+					<li><a href="#">Membership</a></li>
+				</ul>
+			</div>
+			<div class="col-md-3 amet-sed w3ls">
+				
+			</div>
+
+			<div class="col-md-3 amet-sed agileits-w3layouts">
+				
+			</div>
+
+			<div class="col-md-3 amet-sed agileits-w3layouts">
+				<h4>Empire Office</h4>
+				<p>Pejanggik</p>
+				<p>Mataram, Nusa Tenggara Barat</p>
+				<p>Office :  (0370) 902340</p>
+			</div>
+			<div class="clearfix"> </div>
+		</div>
+	</div>
+	<div class="footer-class w3-agile">
+		<p>© 2019. All Rights Reserved</p>
+	</div>
+</div>
+</body>
+</html>
